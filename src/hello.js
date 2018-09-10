80e274d4133fddea9a29d873fd18de0f97eb35d3
mottox2 <y.mottox2@gmail.com>
2018-09-10 21:38:33 +0900

    Use moment

--- src/hello.js
+++ src/hello.js
@@ -1,6 +1,8 @@
+import moment from 'moment'
+
 exports.handler = function(event, context, callback) {
   callback(null, {
     statusCode: 200,
-    body: "Hello, World"
+    body: moment().format()
   });
 }
