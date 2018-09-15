f5aad6778e6e290c007f20acda362a7a78cd65e6
mottox2 <y.mottox2@gmail.com>
2018-09-15 12:25:44 +0900

    Fix args

--- src/args.js
+++ src/args.js
@@ -1,5 +1,6 @@
 exports.handler = function(event, context, callback) {
   console.log(JSON.stringify(event, null, 4))
+  console.log(JSON.stringify(context, null, 4))
   callback(null, {
     statusCode: 200,
     body: "pong",
