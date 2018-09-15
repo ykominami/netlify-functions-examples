e8bcb005e5437324035087363a7338a0c0eb11b0
mottox2 <y.mottox2@gmail.com>
2018-09-15 14:37:34 +0900

    Fix

--- src/args.js
+++ src/args.js
@@ -1,12 +1,5 @@
-// import URLSearchParams from 'url-search-params'
-
 exports.handler = function(event, context, callback) {
   console.log(JSON.stringify(event, null, 4))
-  console.log(event.body)
-  const params = new URLSearchParams(event.body)
-  for (let [key, value] of params.entries()) {
-    console.log(key, value)
-  }
   callback(null, {
     statusCode: 200,
     body: "pong",
